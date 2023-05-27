41965a49c8b6e6cf589ebc14686e5a76e35fea3a
Qlesuga <110699693+Qlesuga@users.noreply.github.com>
2023-05-27 23:25:50 +0200

    long snake

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use piston::event_loop::{EventSettings, Events};
 use piston::input::*;
 use piston::window::WindowSettings;
 
+use std::collections::LinkedList;
+
 const TILE_SIZE: f64 = 20.0;
 
 pub struct App {
@@ -19,12 +21,19 @@ pub struct App {
 
 struct Snake{
     gl: GlGraphics,
-    pos_x: i32,
-    pos_y: i32,
-    direction: Direction
+    snake_parts: LinkedList<Position>,
+    direction: Direction,
+    pos: Position
 }
 
+#[derive(Clone,Copy)]
 struct Direction{
+    x: i32,
+    y: i32
+}
+
+#[derive(Clone,Copy)]
+struct Position{
     x: i32,
     y: i32
 }
@@ -36,18 +45,44 @@ impl Snake{
 
         const RED: [f32;4] = [1.0,0.0,0.0,1.0];
 
-        let square = rectangle::square(self.pos_x as f64 * TILE_SIZE, self.pos_y as f64 * TILE_SIZE, TILE_SIZE);
 
-        self.gl.draw(args.viewport(), | c, gl| {
-            let transform = c.transform;
+        let mut iter = self.snake_parts.iter_mut();
+        while let Some(node) = iter.next() {
 
-            rectangle(RED, square, transform, gl);
-        })
+            let square = rectangle::square(node.x as f64 * TILE_SIZE,node.y as f64 * TILE_SIZE, TILE_SIZE);
+            self.gl.draw(args.viewport(), | c, gl| {
+                let transform = c.transform;
+
+                rectangle(RED, square, transform, gl);
+            })
+        } 
     }
 
     fn update(&mut self, args: &UpdateArgs){
-        self.pos_x += self.direction.x;
-        self.pos_y += self.direction.y;
+        self.pos.x += self.direction.x;
+        self.pos.y += self.direction.y; 
+        
+        let mut iter = self.snake_parts.iter_mut();
+        if let Some(mut prev_node) = iter.next() {
+            while let Some(node) = iter.next() {
+                *node = std::mem::replace(&mut prev_node, *node);
+            }
+        }
+
+        if let Some(mut node) = self.snake_parts.front_mut() {
+            *node = Position{x:self.pos.x,y:self.pos.y};
+        }
+
+    }
+
+    fn pressed(&mut self, btn: &Button){
+        self.direction = match btn {
+            &Button::Keyboard(Key::Up) if self.direction.y != 1  => Direction{x:0,y:-1},
+            &Button::Keyboard(Key::Down) if self.direction.y != -1 => Direction{x:0,y:1},
+            &Button::Keyboard(Key::Left) if self.direction.x != 1 => Direction{x:-1,y:0},
+            &Button::Keyboard(Key::Right) if self.direction.x != -1 => Direction{x:1,y:0},
+            _ => self.direction
+        };
     }
 }
 
@@ -63,7 +98,7 @@ impl App {
             graphics::clear(GREEN, gl);
         });
 
-        self.snake.render(args)
+        self.snake.render(args);
     }
 
     fn update(&mut self, args: &UpdateArgs){
@@ -71,53 +106,43 @@ impl App {
     }
 
     fn pressed(&mut self, btn: &Button){
-        self.snake.direction = match btn {
-            &Button::Keyboard(Key::Up) if self.snake.direction.y != 1  => Direction{x:0,y:-1},
-            &Button::Keyboard(Key::Down) if self.snake.direction.y != -1 => Direction{x:0,y:1},
-            &Button::Keyboard(Key::Left) if self.snake.direction.x != 1 => Direction{x:-1,y:0},
-            &Button::Keyboard(Key::Right) if self.snake.direction.x != -1 => Direction{x:1,y:0},
-            _ => Direction{x: self.snake.direction.x,y: self.snake.direction.y}
-        };
+        self.snake.pressed(btn);
     }
 }
 
 fn main() {
     let opengl = OpenGL::V3_2;
 
-    let mut window: Window = WindowSettings::new("snake", [200, 200])
+    let mut window: Window = WindowSettings::new("snake", [700, 700])
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
         .unwrap();
 
-        let mut app = App {
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+        snake: Snake{
             gl: GlGraphics::new(opengl),
-            snake: Snake{
-                gl: GlGraphics::new(opengl),
-                pos_x: 4,
-                pos_y: 4,
-                direction: Direction{
-                    x: 1,
-                    y: 0
-                }
-            }
-        };
+            pos: Position{x:4,y:4},
+            snake_parts: LinkedList::from([Position{x:4,y:4},Position{x:3,y:4},Position{x:2,y:4}]),
+            direction: Direction{x:1,y:0}
+        }
+    };
 
+    let mut events = Events::new(EventSettings::new().ups(8));
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            app.render(&args);
+        }
 
-        let mut events = Events::new(EventSettings::new().ups(8));
-        while let Some(e) = events.next(&mut window) {
-            if let Some(args) = e.render_args() {
-                app.render(&args);
-            }
+        if let Some(args) = e.update_args() {
+            app.update(&args)
+        }
 
-            if let Some(args) = e.update_args() {
-                app.update(&args)
-            }
-
-            if let Some(args) = e.button_args() {
-                if args.state == ButtonState::Press {
-                    app.pressed(&args.button);
-                }
+        if let Some(args) = e.button_args() {
+            if args.state == ButtonState::Press {
+                app.pressed(&args.button);
             }
         }
+    }
 }
